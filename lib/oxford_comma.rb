ea1418a9320b0d744bea04555239dd0386bcdbd1
Natasha <natasha.khoe@gmail.com>
2022-11-01 19:53:50 +1100

    commit tree 93743ff333fcb9190518f2142a45b8133896bc35

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length == 2
+        array.insert(-2, "and")
+        array.join(" ")
+    elsif array.length >=3
+        last_item = array[-1]
+        array.insert(-2, "and ").pop
+        array.join(", ") << last_item
+    else
+        array[0]
+    end
 end
